8056b65e8bbd906b52ed346a37c42a84e8fb75b4
shenxiangwei <1037323401@qq.com>
2019-10-01 22:01:33 +0800

    添加注释

--- src/test/java/demo/read/IndexOrNameData.java
+++ src/test/java/demo/read/IndexOrNameData.java
@@ -10,6 +10,13 @@ import lombok.Data;
  **/
 @Data
 public class IndexOrNameData {
+
+    /**
+     *
+     * 注意:如果表头存在问题,使用name去读可能会存在问题;
+     *
+     */
+
     /**
      * 强制读取第三个 这里不建议 index 和 name 同时用，要么一个对象只用index，要么一个对象只用name去匹配
      */
